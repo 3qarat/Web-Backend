cbd7f5f97bd5b89fbd0a8991aa995473bf7451d5
iamhashemtarek <hashemtarek2012@gmail.com>
2024-04-21 17:18:46 +0200

    hello word

---
+++ src/server/config/config.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+
+const { NODE_ENV, PORT } = process.env;
+
+export default {
+  NODE_ENV,
+  PORT,
+};
---
+++ src/server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import config from "./config/config.js";
+const app = express();
+
+//mw
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.json({
+    status: "success",
+    message: "hello world",
+  });
+});
+
+const PORT = config.PORT;
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
