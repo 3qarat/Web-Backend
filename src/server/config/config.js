a7c3f45dcb189d12a3ef8940eb911bedeceb9fb2
iamhashemtarek <hashemtarek2012@gmail.com>
2024-04-23 16:44:28 +0200

    add mysql, adminer services to docker-compose file & configure connction pooling to mysql

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,8 +1,19 @@
 import "dotenv/config";
 
-const { NODE_ENV, PORT } = process.env;
+const {
+  NODE_ENV,
+  PORT,
+  MYSQL_ROOT_PASSWORD,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+} = process.env;
 
 export default {
   NODE_ENV,
   PORT,
+  MYSQL_ROOT_PASSWORD,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import config from "./config/config.js";
+
 const app = express();
 
 //mw
 app.use(express.json());
 
-app.get("/", (req, res, next) => {
+app.get("/", async (req, res, next) => {
+  const result = await pool.query("show tables");
   res.json({
     status: "success",
     message: "hello world",
