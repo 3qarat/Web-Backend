d933309c60ff9219173b1517882ed113c102c86b
iamhashemtarek <hashemtarek2012@gmail.com>
2024-04-30 13:47:56 +0300

    check availability & update apartment status after a tx compeletion

--- src/server/api/resources/transaction/transaction.service.js
+++ src/server/api/resources/transaction/transaction.service.js
@@ -31,19 +31,27 @@ export const createTransaction = async ({
       throw new AppError("tenant not found", 404);
     }
 
-    //check if apartment exists
-    sql = "select 1 from apartment where id = ?";
+    //check if apartment exists and availability
+    sql = "select status from apartment where id = ?";
     const [apartments] = await connection.query(sql, [apartment_id]);
     if (apartments.length === 0) {
       throw new AppError("apartment not found", 404);
     }
+    if (apartments[0].status != "available") {
+      throw new AppError(`apartment is not available`, 400);
+    }
+
+    // update apartment status
+    const newStatus = tx_type === "rental" ? "rented" : "sold";
+    sql = "update apartment set status = ? where id = ?";
+    await connection.query(sql, [newStatus, apartment_id]);
 
     // record tx
     sql = `
     insert into transactions(tx_type, price, payment_method, apartment_id, user_id)
     values (?, ?, ?, ?, ?)
   `;
-    const [result] = await pool.query(sql, [
+    const [result] = await connection.query(sql, [
       tx_type,
       price,
       payment_method,
