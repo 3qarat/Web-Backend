f4881e2e27da0d4b10f9df650760845276a1298b
iamhashemtarek <hashemtarek2012@gmail.com>
2024-06-26 21:35:12 +0300

    config for deploy

--- src/server/api/database/index.js
+++ src/server/api/database/index.js
@@ -1,11 +1,27 @@
 import mysql from "mysql2/promise";
 import config from "../../config/config.js";
 
+// const pool = mysql.createPool({
+//   host: config.MYSQL_HOST,
+//   user: config.MYSQL_USER,
+//   password: config.MYSQL_PASSWORD,
+//   database: config.MYSQL_DATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+//   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+//   queueLimit: 0,
+//   enableKeepAlive: true,
+//   keepAliveInitialDelay: 0,
+// });
+
 const pool = mysql.createPool({
-  host: config.MYSQL_HOST,
-  user: config.MYSQL_USER,
-  password: config.MYSQL_PASSWORD,
-  database: config.MYSQL_DATABASE,
+  host: "viaduct.proxy.rlwy.net",
+  user: "root",
+  password: "LluaADqlAXpEiOCFbjwSYUigajcvhNin",
+  database: "railway",
+  port: 54933,
+  uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
@@ -14,6 +30,7 @@ const pool = mysql.createPool({
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
 });
+
 
 
 pool.on("connection", () =>
