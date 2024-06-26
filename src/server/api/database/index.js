1e2bb34951d8e633594dc21df2daf2027d26d7a2
iamhashemtarek <hashemtarek2012@gmail.com>
2024-06-26 22:28:05 +0300

    db issue

--- src/server/api/database/index.js
+++ src/server/api/database/index.js
@@ -23,10 +23,10 @@ const pool = mysql.createPool({
   port: 54933,
   uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
   waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
+  //connectionLimit: 10,
+  //maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  //idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  //queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
 });
