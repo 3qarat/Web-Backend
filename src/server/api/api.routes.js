b153d9fdbca295ca9c92738aba0885c982e88b82
iamhashemtarek <hashemtarek2012@gmail.com>
2024-06-26 11:10:57 +0300

    from auth to user.routes

--- src/server/api/api.routes.js
+++ src/server/api/api.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import authRoutes from "./resources/user/auth.routes.js";
+import userRoutes from "./resources/user/user.routes.js";
 import apartmentRoutes from "./resources/apartment/apartment.routes.js";
 import transactionRoutes from "./resources/transaction/transaction.routes.js";
 
 const restRouter = new Router();
 
-restRouter.use("/v1/user", authRoutes);
+restRouter.use("/v1/user", userRoutes);
 restRouter.use("/v1/apartment", apartmentRoutes);
 restRouter.use("/v1/transaction", transactionRoutes);
 
