570999b34dd41442a6ad2a7f0f390640b0d369d9
iamhashemtarek <hashemtarek2012@gmail.com>
2024-06-26 22:30:46 +0300

    railway issue

--- src/server/api/database/index.js
+++ src/server/api/database/index.js
@@ -22,13 +22,13 @@ const pool = mysql.createPool({
   database: "railway",
   port: 54933,
   uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
-  waitForConnections: true,
+  //waitForConnections: true,
   //connectionLimit: 10,
   //maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
   //idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   //queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+  //enableKeepAlive: true,
+ // keepAliveInitialDelay: 0,
 });
 
 
