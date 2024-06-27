d4c5f83ba46e14af13ce6f83a4bff6eaf5843644
iamhashemtarek <hashemtarek2012@gmail.com>
2024-06-27 11:09:36 +0300

    dynamic db conn config

--- src/server/api/database/index.js
+++ src/server/api/database/index.js
@@ -1,6 +1,3 @@
-// import mysql from "mysql2/promise";
-// import config from "../../config/config.js";
-
 // const pool = mysql.createPool({
 // host: config.MYSQL_HOST,
 // user: config.MYSQL_USER,
@@ -15,61 +12,42 @@
 // keepAliveInitialDelay: 0,
 // });
 
-// // const pool = mysql.createPool({
-// host: "viaduct.proxy.rlwy.net",
-// user: "root",
-// password: "LluaADqlAXpEiOCFbjwSYUigajcvhNin",
-// database: "railway",
-// port: 54933,
-// uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
-// waitForConnections: true,
-// connectionLimit: 10,
-// maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-// idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-// queueLimit: 0,
-// enableKeepAlive: true,
-// keepAliveInitialDelay: 0,
-// // });
-
-// pool.on("acquire", () => {
-//   console.log("Connection acquired");
-// });
-
-// pool.on("connection", () => {
-//   console.log(`Connected to ${config.MYSQL_DATABASE}`);
-// });
-
-// pool.on("release", () => {
-//   console.log("Connection released");
-// });
-
-// pool.on("enqueue", () => {
-//   console.log("Waiting for available connection slot");
-// });
-
-// export default pool;
-
 import mysql from "mysql2/promise";
 import config from "../../config/config.js";
 
 let pool;
 
+const devConfig = {
+  host: config.MYSQL_HOST,
+  user: config.MYSQL_USER,
+  password: config.MYSQL_PASSWORD,
+  database: config.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+};
+
+const prodConfig = {
+  host: "viaduct.proxy.rlwy.net",
+  user: "root",
+  password: "LluaADqlAXpEiOCFbjwSYUigajcvhNin",
+  database: "railway",
+  port: 54933,
+  uri: "mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+};
 const initializePool = () => {
-  pool = mysql.createPool({
-    host: "viaduct.proxy.rlwy.net",
-    user: "root",
-    password: "LluaADqlAXpEiOCFbjwSYUigajcvhNin",
-    database: "railway",
-    port: 54933,
-    uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-  });
+  pool = mysql.createPool(config.NODE_ENV == 'development' ? devConfig : prodConfig);
 
   pool.on("acquire", () => {
     console.log("Connection acquired");
@@ -116,4 +94,3 @@ process.on("SIGINT", gracefulShutdown);
 process.on("SIGTERM", gracefulShutdown);
 
 export default pool;
-
