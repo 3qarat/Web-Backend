30d6453055b17c2b0986efeaa3ec10a7d7c6bd8e
iamhashemtarek <hashemtarek2012@gmail.com>
2024-06-27 10:28:02 +0300

    update db conn config

--- src/server/api/database/index.js
+++ src/server/api/database/index.js
@@ -1,5 +1,5 @@
-import mysql from "mysql2/promise";
-import config from "../../config/config.js";
+// import mysql from "mysql2/promise";
+// import config from "../../config/config.js";
 
 // const pool = mysql.createPool({
 //   host: config.MYSQL_HOST,
@@ -15,13 +15,13 @@ import config from "../../config/config.js";
 //   keepAliveInitialDelay: 0,
 // });
 
-const pool = mysql.createPool({
-  host: "viaduct.proxy.rlwy.net",
-  user: "root",
-  password: "LluaADqlAXpEiOCFbjwSYUigajcvhNin",
-  database: "railway",
-  port: 54933,
-  uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
+// // const pool = mysql.createPool({
+  // host: "viaduct.proxy.rlwy.net",
+  // user: "root",
+  // password: "LluaADqlAXpEiOCFbjwSYUigajcvhNin",
+  // database: "railway",
+  // port: 54933,
+  // uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
   //waitForConnections: true,
   //connectionLimit: 10,
   //maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
@@ -29,13 +29,91 @@ const pool = mysql.createPool({
   //queueLimit: 0,
   //enableKeepAlive: true,
  // keepAliveInitialDelay: 0,
-});
+// // });
+
+// pool.on("acquire", () => {
+//   console.log("Connection acquired");
+// });
+
+// pool.on("connection", () => {
+//   console.log(`Connected to ${config.MYSQL_DATABASE}`);
+// });
+
+// pool.on("release", () => {
+//   console.log("Connection released");
+// });
+
+// pool.on("enqueue", () => {
+//   console.log("Waiting for available connection slot");
+// });
 
 
+// export default pool;
 
-pool.on("connection", () =>
-  console.log(`connected to ${config.MYSQL_DATABASE}`)
-);
-pool.on("release", () => console.log("connection is released"));
+import mysql from "mysql2/promise";
+import config from "../../config/config.js";
+
+let pool;
+
+const initializePool = () => {
+  pool = mysql.createPool({
+    host: "viaduct.proxy.rlwy.net",
+    user: "root",
+    password: "LluaADqlAXpEiOCFbjwSYUigajcvhNin",
+    database: "railway",
+    port: 54933,
+    uri:"mysql://root:LluaADqlAXpEiOCFbjwSYUigajcvhNin@mysql.railway.internal:3306/railway",
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+   keepAliveInitialDelay: 0,
+  });
+
+  pool.on("acquire", () => {
+    console.log("Connection acquired");
+  });
+
+  pool.on("connection", () => {
+    console.log(`Connected to ${config.MYSQL_DATABASE}`);
+  });
+
+  pool.on("release", () => {
+    console.log("Connection released");
+  });
+
+  pool.on("enqueue", () => {
+    console.log("Waiting for available connection slot");
+  });
+
+  pool.on('error', handlePoolError);
+};
+
+const handlePoolError = (err) => {
+  console.error('MySQL pool error: ', err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
+    console.error('Reinitializing MySQL connection pool');
+    initializePool();
+  } else {
+    throw err;
+  }
+};
+
+// Initialize the pool at the start
+initializePool();
+
+// Graceful shutdown
+const gracefulShutdown = () => {
+  pool.end((err) => {
+    if (err) console.error('Error closing pool: ', err);
+    else console.log('Pool closed');
+    process.exit(0);
+  });
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
 
 export default pool;
